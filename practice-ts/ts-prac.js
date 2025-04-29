6b2c6b0f8c28c876aa19274c374a1c2d194605fc
최현준 <choijjunii1@gmail.com>
2025-04-29 13:07:47 +0900

    TypeScript 환경셋팅 다시하기

---
+++ practice-ts/ts-prac.js
@@ -0,0 +1,5 @@
+"use strict";
+function joon(name, age, job) {
+    return name + age + job;
+}
+console.log(joon("joon", 25, "developer"));
---
+++ practice-ts/ts-prac.ts
@@ -0,0 +1,7 @@
+function joon(name: string, age: number, job: string) {
+
+  return name + age + job;
+
+}
+
+console.log(joon("joon", 25, "developer"));
