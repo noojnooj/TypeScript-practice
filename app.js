dadd40c98572eeaa9236410f922908b272db3fcc
최현준 <choijjunii1@gmail.com>
2025-04-30 09:32:17 +0900

    ec2 올려보기

---
+++ app.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    return a + b;
+}
+function minus(a, b) {
+    return a - b;
+}
+console.log(add(3, 5));
+console.log(minus(8, 2));
---
+++ app.ts
@@ -0,0 +1,14 @@
+function add(a: number, b: number): number {
+  
+  return a + b;
+
+}
+
+function minus(a: number, b: number): number {
+  
+  return a - b;
+
+}
+
+console.log(add(3, 5));
+console.log(minus(8, 2));
