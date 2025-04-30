7e0adfc4a6e35613f427eaf69fa6f129e338fba3
최현준 <choijjunii1@gmail.com>
2025-04-30 09:25:07 +0900

    브랜치생성

--- practice-ts/ts-prac.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function joon(name, age, job) {
-    return name + age + job;
-}
-console.log(joon("joon", 25, "developer"));
--- practice-ts/ts-prac.ts
+++
@@ -1,7 +0,0 @@
-function joon(name: string, age: number, job: string) {
-
-  return name + age + job;
-
-}
-
-console.log(joon("joon", 25, "developer"));
